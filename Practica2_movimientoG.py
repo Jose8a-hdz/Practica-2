c4b98434dc748794db505b581dd19de7c51dfca3
Jose8a-hdz <al23760815@ite.edu.mx>
2025-09-18 22:01:52 -0700

    cambiar tamano y forma

--- Practica2_movimientoG.py
+++ Practica2_movimientoG.py
@@ -6,50 +6,44 @@ ventana.setup(width=600, height=600)
 
 # Crear turtle para dibujar
 t = turtle.Turtle()
-t.hideturtle()  # Ocultar el turtle, solo veremos el cuadrado
+t.hideturtle()  # Ocultar el turtle, solo veremos el circulo
 t.penup()
-t.speed(10)
+t.speed(0)
 
 # Posición inicial
 x, y = 0, 0
 
-# Función para dibujar el cuadrado
-def cuadrado():
-    t.clear()  # Borrar el cuadrado anterior
+# Función para dibujar el circulo
+def circulo():
+    t.clear()  # Borrar el circulo anterior
     t.goto(x, y)
     t.pendown()
-    t.forward(30)  # Mover un poco para que el cuadrado no se dibuje en el centro
-    t.right(90)
-    t.forward(30)  # Mover un poco para que el cuadrado no se dibuje en el centro
-    t.right(90)
-    t.forward(30)  # Mover un poco para que el cuadrado no se dibuje en el centro
-    t.right(90)
-    t.forward(30)  # Mover un poco para que el cuadrado no se dibuje en el centro
+    t.circle(50)
     t.penup()
 
-# Dibujar cuadrado inicial
-cuadrado()
+# Dibujar circulo inicial
+circulo()
 
 # Funciones de movimiento
 def arriba():
     global y
     y += 10
-    cuadrado()
+    circulo()
 
 def abajo():
     global y
     y -= 10
-    cuadrado()
+    circulo()
 
 def izquierda():
     global x
     x -= 10
-    cuadrado()
+    circulo()
 
 def derecha():
     global x
     x += 10
-    cuadrado()
+    circulo()
 
 # Enlazar teclas
 ventana.listen()
